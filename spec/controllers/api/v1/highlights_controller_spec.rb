5477db7d992c7e120009bc8c118c32ae7af8c545
Mohammed O. Tillawy <tillawy@gmail.com>
2015-10-21 08:59:35 +0300

    new test for query hilights

--- spec/controllers/api/v1/highlights_controller_spec.rb
+++ spec/controllers/api/v1/highlights_controller_spec.rb
@@ -4,27 +4,46 @@ RSpec.describe "HighlightsController", :type => :request do
 
   describe "JSON Api" do
 
-   describe "New Highlights" do 
-     it "should insert a new highlight" do 
-     end
-   end
+    describe "New Highlights" do 
+      it "should insert a new highlight" do 
+      end
+    end
 
    describe "Query Highlights" do 
 
     it "should query with web site" do 
-      user = FactoryGirl.create(:user, email: "asdf@domain.com")
-      highlight = FactoryGirl.create(:highlight, user: user)
-      headers = {}
-      headers["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{user.email}:#{user.password}")
-      get '/api/highlights.json', {}, headers
-      expect(response).to be_success
-      expect(response).to render_template("highlights/index")
-      json = JSON.parse(response.body)
-      expect(json.length).to eq(1)
-      expect(json[0]['id']).to eq(highlight.id)
+        user = FactoryGirl.create(:user, email: "asdf@domain.com")
+        highlight = FactoryGirl.create(:highlight, user: user)
+        headers = {}
+        headers["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{user.email}:#{user.password}")
+        get '/api/highlights.json', {}, headers
+        expect(response).to be_success
+        expect(response).to render_template("highlights/index")
+        json = JSON.parse(response.body)
+        expect(json.length).to eq(1)
+        expect(json[0]['id']).to eq(highlight.id)
 
+      end
     end
-   end
+
+
+
+    describe "Query Highlights" do 
+      it "should query highlights using host,path,port,is_https" do 
+        user = FactoryGirl.create(:user, email: "asdf@domain.com")
+        headers = {}
+        headers["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{user.email}:#{user.password}")
+        get '/api/highlights.json', {}, headers
+        expect(response).to be_success
+        expect(response).to render_template("highlights/index")
+        json = JSON.parse(response.body)
+        expect(json.length).to eq(1)
+        expect(json[0]['id']).to eq(highlight.id)
+
+      end
+    end
+
+
 
   end
 
