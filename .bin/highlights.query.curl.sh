653cfd4384f3a6687232ce0eb893826c91a3642b
Mohammed O. Tillawy <tillawy@gmail.com>
2015-10-23 19:11:39 +0300

    ignore docs and shippable dirs

---
+++ .bin/highlights.query.curl.sh
@@ -0,0 +1,7 @@
+PORT="9002";
+curl -v \
+	--user "tillawy@me.com:password" \
+	-H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+	http://localhost:${PORT}/highlights/query.json
+
